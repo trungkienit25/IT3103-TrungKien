4545908a5f6fbc3db1fd1e3f156d164134f9b1b7
trungkienit25 <162586320+trungkienit25@users.noreply.github.com>
2024-11-23 07:30:24 +0700

    Restructure

--- Nguyen_Trung_Kien_20226110_LAB1/FirstProgram/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// Nguyen Trung Kien 20226110
-// Chuong trinh in ra man hinh dong chu theo yeu cau
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("(Xin chao \n cac ban)");
-        System.out.println("(Hello \n World)");
-    }
-}
